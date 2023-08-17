ba0ebd6fa4b5dbf2f5a38dfd06732e3583960b6f
sivakrays <siva.krays@gmail.com>
2023-08-17 12:52:32 +0530

    Demo commite

--- src/main/java/com/example/demo/OtpApplication.java
+++ src/main/java/com/example/demo/OtpApplication.java
@@ -2,10 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class OtpApplication {
 
 	public static void main(String[] args) {
