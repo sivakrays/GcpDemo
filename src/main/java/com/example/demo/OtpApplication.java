c5f8543d37cb032d7b0fc542c7e8dd93e8475ba7
sivakrays <siva.krays@gmail.com>
2023-10-16 14:34:17 +0530

    docker file added for cloud deploy

--- src/main/java/com/example/demo/OtpApplication.java
+++ src/main/java/com/example/demo/OtpApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OtpApplication {
 
-	public String PORT = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(OtpApplication.class, args);
